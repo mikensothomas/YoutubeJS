85835d8845ee748520a258bbf13e0e05440b5174
mikensothomas <thomasmikenson40@gmail.com>
2023-01-09 02:24:54 -0300

    C

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
+import madificador from "./madificador.js";
+
 var ingredientes = ['mel', 'agua', 'sal', 'mostarda'];
-
-function capitalizado(ingredientes){
-
-    var modificado = [];
-
-    for ( var i = 0; i < ingredientes.length; i++){
-        var letraInicial = ingredientes[i].charAt(0).toUpperCase();
-        var restoTexto = ingredientes[i].slice(1);
-        var resultado = letraInicial + restoTexto;
-        console.log(resultado);
-    
-        modificado[i] = resultado;
-    }
-    return modificado;
-}
-
-function ordenar(ingredientes){
-    var ordenado = ingredientes.sort(function(a,b){
-        return a.localeCompare(b);
-    });
-    return ordenado;
-}
-
-var resultadoCaptalizado = capitalizado(ingredientes);
-var resultadoOrdenado = ordenar(resultadoCaptalizado);
+var resultadoCaptalizado = madificador.capitalizar(ingredientes);
+var resultadoOrdenado = madificador.ordenar(resultadoCaptalizado);
+var restoCaixaAlta = madificador.caixaAlta(ingredientes);
 
 console.log(resultadoCaptalizado);
-//console.log(resultadoOrdenado);
+console.log(resultadoOrdenado);
+console.log(restoCaixaAlta);
